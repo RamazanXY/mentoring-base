5138c0a068d8c86869ad1bb3ef55c9b1caeb5306
RamazanXY <ccgibadatovacc@gmail.com>
2025-01-23 10:53:49 +0500

    Added components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { UsersListComponent } from './user-list/user-list.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+{ path: '', component: MainComponent },
+{ path: 'users', component: UsersListComponent },
+{ path: '**', redirectTo: '' }
+];
+
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'app-users-list',
+    standalone: true,
+    imports: [],
+    templateUrl: './user-list.component.html',
+    styleUrl: './user-list.component.scss'
+})
+
+export class UsersListComponent {
+
+}
