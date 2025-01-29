3c19e3370270da54006d3a4ec9efdfcb17743eae
RamazanXY <ccgibadatovacc@gmail.com>
2025-01-29 13:31:06 +0500

    made user cards

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,13 +1,37 @@
-import { Component } from "@angular/core";
+import { NgFor } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, Injectable } from "@angular/core";
 
-@Component ({
+export interface Users {
+    
+}
+
+@Component({
     selector: 'app-users-list',
     standalone: true,
-    imports: [],
+    imports: [NgFor],
     templateUrl: './user-list.component.html',
     styleUrl: './user-list.component.scss'
 })
 
-export class UsersListComponent {
 
+export class UsersListComponent {
+    readonly ApiService = inject(HttpClient);
+    users:any = [];
+
+    constructor() {
+        this.ApiService.get('https://jsonplaceholder.typicode.com/users').subscribe(
+            (response: any) => {
+              this.users = response;
+              console.log('USERS:',this.users)
+            }
+        )
+    }
+
+    deleteUser(id:number) {
+        this.users = this.users.filter(
+            // @ts-ignore
+            item => item.id !== id
+        )
+    }
 }
